2927626f96b6491b21f903f0640db6af0df19cf0
Seraph919 <167767889+Seraph919@users.noreply.github.com>
2024-08-09 21:03:33 +0100

    Update ft_recursive_power.c

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -10,13 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_power(int nb, int power)
+int	ft_recursive_power(int nb, int power)
 {
-	if (nb == 0 && power == 0)
-		return (1);
+    if (power < 0)
+        return 0;
 	if (power == 0)
-	{
 		return (1);
-	}
-	return (nb * ft_iterative_power(nb, power - 1));
+	return (nb * ft_recursive_power(nb, power - 1));
 }
